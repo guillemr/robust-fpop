b8fe57973a613d5f8d1b7445b55d28a74b99251a
guillemr <guillemr@users.noreply.github.com>
2017-07-07 14:40:35 +0200

    Modified the README.Rmd and update head of c files

--- src/fp_CFunctPart.h
+++ src/fp_CFunctPart.h
@@ -1,13 +1,13 @@
-/* February 2015 Guillem Rigaill <rigaill@evry.inra.fr> 
+/* July 2017 Guillem Rigaill <guillem.rigaill@inra.fr> 
 
    This file is part of the R package Robseg
 
-   opfp is free software; you can redistribute it and/or modify
+   robseg is free software; you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License (LGPL) as published by
    the Free Software Foundation; either version 2.1 of the License, or
    (at your option) any later version.
    
-   opfp is distributed in the hope that it will be useful,
+   robseg is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
--- src/fp_Rob_fpop.h
+++ src/fp_Rob_fpop.h
@@ -1,3 +1,22 @@
+/* July 2017 Guillem Rigaill <guillem.rigaill@inra.fr> 
+
+   This file is part of the R package Robseg
+
+   robseg is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License (LGPL) as published by
+   the Free Software Foundation; either version 2.1 of the License, or
+   (at your option) any later version.
+   
+   robseg is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public License
+   along with opfp; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef ROB_FPOPH
 #define ROB_FPOPH
 

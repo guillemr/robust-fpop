dbb4e4f791faf80dfc7c4752986b5470eb4229d3
guillemr <guillem.rigaill@inra.fr>
2018-04-09 09:34:13 +0200

    add binseg in robseg

--- src/Node.h
+++ src/Node.h
@@ -1,13 +1,13 @@
-/* July 2017 Guillem Rigaill <guillem.rigaill@inra.fr> 
+/* February 2014 Guillem Rigaill <rigaill@evry.inra.fr> 
 
-   This file is part of the R package Robseg
+   This file is part of the R package fpop
 
-   robseg is free software; you can redistribute it and/or modify
+   fpop is free software; you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License (LGPL) as published by
    the Free Software Foundation; either version 2.1 of the License, or
    (at your option) any later version.
    
-   robseg is distributed in the hope that it will be useful,
+   opfp is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
@@ -17,13 +17,27 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef ROB_FPOPH
-#define ROB_FPOPH
+#ifndef _Node_h_
+#define _Node_h_
 
-#include "fp_liste.h"
 
-void rob_fpop(const double *profil, const int *nbi, const double *lambda_, 
-		const double *lthreshold_, const double *rthreshold_,
-		const double *lslope_, const double *rslope_,
-		const double *mini, const double *maxi, int *origine, double *cout_n, double *mean_n);
+class Node
+{
+public:
+	int Index;
+	double Value;
+	int LowIndex;
+	int HighIndex;
+	Node(int I, double V, int LI, int HI);
+	Node();
+	bool operator<(Node &Other);
+	bool operator<=(Node &Other);
+	Node operator=(Node &Other);
+};
+
+void Swap(Node &a, Node &b);
+
+
 #endif
+
+
